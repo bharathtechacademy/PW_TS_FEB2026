690582f8dd65ec95d6f6b2019aee1c4140262cc7
bharathautomation <139088966+bharathtechacademy@users.noreply.github.com>
2026-03-18 09:12:10 +0530

    Async

---
+++ typescript-training/oops/oops.ts
@@ -0,0 +1,12 @@
+//1. classes
+//2. objects
+//3. constructors
+//4. inheritance
+//5. polymorphism
+//6. abstraction
+//7. encapsulation
+//8. static members
+//9. access modifiers
+//10. interfaces
+//11. abstract classes
+//12. this & super keywords
